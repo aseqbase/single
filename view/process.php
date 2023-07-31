e927bd056a2bdb482b2fe69332287d188b1c7715
Mohammad Fathi <40602800+mimfa@users.noreply.github.com>
2023-07-31 19:36:44 +0330

    Create process.php

---
+++ view/process.php
@@ -0,0 +1,6 @@
+<?php REGION("initial"); ?>
+    <title><?php echo \_::$INFO->FullName; ?></title>
+    <link rel="icon" href="<?php echo forceFullUrl(\_::$INFO->LogoPath); ?>">
+<?php REGION("body"); ?>
+    <?php PAGE("home"); ?>
+<?php REGION("final"); ?>
