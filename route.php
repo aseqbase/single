b7057a97c9b6fe59202a8fa94cd1d49cf4ffdad1
Mohammad Fathi <mimfapost@gmail.com>
2025-10-17 01:21:39 +0330

    v4.9.9

--- route.php
+++ route.php
@@ -1,13 +1,14 @@
 <?php
-
-// To unset all exist routers sat at the bottom layers
-\_::$Aseq->Reset();
+// To unset the default router sat at the bottom layers
+\_::$Aseq->On()->Reset();
 
 /**
  * Use your routers by below formats
  * \_::$Aseq->On("A Part Of Path?")->Default("Route Name");
+ * Or use a suitable handler for example
+ * \_::$Aseq->On()->Default(fn($router)=>response(\MiMFa\Library\Html::ExternalHeading("Hello World!")));
  */
 
-// To route all requests to the DefaultRouteName
+// To route other requests to the DefaultRouteName
 \_::$Aseq->On()->Default(\_::$Config->DefaultRouteName);
 ?>
